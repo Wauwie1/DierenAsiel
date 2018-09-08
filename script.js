3010954967e3f7d5ce4327c525fdde6cecc09d7f
Paul <paulusman@live.nl>
2018-09-08 20:00:52 +0200

    Displays if username and regnr are entered correctly

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
 $( document ).ready(function() {
     console.log("DOM loaded");
+
+  
+
+
+
+
+
+
+
+
     $('h1').addClass('titel');
-    $('#naam').after('<p class="hidden" id="popup">Verplicht veld.</p>');
+    $('#naam').after('<p class="hidden" id="popupNaam">Verplicht veld.</p>');
+    $('#regnr').after('<p class="hidden" id="popupReg">Incorrect registratienummer.</p>');
 
 
-    
-      $('#naam').blur(function(){
-        $('.hidden').css({'visibility': 'visible'});
-      });
-      
-      $('#naam').focus(function(){
-        $('.hidden').css({'visibility': 'hidden'});
-      });
+      //Dispaly hidden functions
+      function HideHidden(id){
+        $('#' + id).addClass('hidden');
+      }
+      function ShowHidden(id){
+        $('#' + id).removeClass('hidden');
+      }
 
+      //Submit button click
       function OnSubmit(event){
-        alert( "Handler for .click() called.");
+        var animal = {
+          'naam': $('#naam').val(),
+          'leeftijd': $('#leeftijd').val(),
+          'regnr': $('#regnr').val(),
+          'gereserveerd': $('input[name=gereserveerd]:checked').val(),
+          'hondkat': $('input[name=hondkat]:checked').val()
+        };
+
+        if(animal.naam.length == 0){
+          ShowHidden('popupNaam');
+        }else{
+          HideHidden('popupNaam');
+        }
+
+       if(animal.regnr < 1000){
+          ShowHidden('popupReg');
+        }else{
+          HideHidden('popupReg');
+        }
+
       }
       $('#btn_submit').click(OnSubmit);
 
